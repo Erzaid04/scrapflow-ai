c92ccdbc84be54b5b2319b504e2f25c867948208
Erzaid04 <erzaidkakar@gmail.com>
2026-06-15 12:37:14 +0530

    feat: implement JWT auth and protected route foundation

---
+++ backend/app/dependencies/auth.py
@@ -0,0 +1,42 @@
+from fastapi import Depends,HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import session
+
+from app.auth.jwt_handler import verify_access_token
+from app.database.connection import SessionLocal
+from app.models.user import User
+
+#Oauth2 dependency
+oauth2_schema = OAuth2PasswordBearer(
+    tokenUrl="/api/v1/auth/login"
+)
+
+#database dependency
+def get_db():
+    db = SessionLocal()
+    
+    try:
+        yield db
+    finally:
+        db.close()
+        
+def get_current_user(
+    token: str = Depends(oauth2_schema),
+    db:session = Depends(get_db)
+):
+    try:
+        payload = verify_access_token(token)
+        user_id = payload["sub"]
+        user = db.query(User).filter(User.id == user_id).first()
+        
+        if not user:
+            raise HTTPException(
+                status_code = 401,
+                detail="User not found"
+            )
+        return user
+    except Exception:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token"
+        )
