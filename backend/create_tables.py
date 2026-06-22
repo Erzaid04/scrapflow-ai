a2b220a164e59f9e987d3563caac1e6c5fd1e596
Erzaid04 <erzaidkakar@gmail.com>
2026-06-22 15:01:54 +0530

    feat: add transaction model foundation

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -3,6 +3,7 @@ from app.models.base import Base
 #import models so SQL ALchemy can discover them
 from app.models.user import User
 from app.models.inventory import Inventory
+from app.models.transaction import Transaction
 print("creating tables...")
 Base.metadata.create_all(bind= engine)
 print("Tables created successfully")
