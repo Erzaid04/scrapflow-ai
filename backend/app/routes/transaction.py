7d4932aa28d7ad6ebd0a511fb318f3f6d9f4a527
Erzaid04 <erzaidkakar@gmail.com>
2026-06-23 10:45:28 +0530

    feat: complete transaction management module

--- backend/app/routes/transaction.py
+++ backend/app/routes/transaction.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import Session
 from app.schemas.transaction import (TransactionCreate, TransactionResponse)
 from app.services.transaction_service import create_transaction
 from app.dependencies.roles import get_current_user
+from app.dependencies.roles import require_roles
+
 
 router = APIRouter()
 
@@ -11,11 +13,17 @@ router = APIRouter()
 def create_new_transaction(
     transaction_data:TransactionCreate,
     db:Session = Depends(get_db),
-    current_user = Depends(get_current_user)
+    current_user = Depends(
+        require_roles(
+            "owner",
+            "worker"
+        ))
+    
     
 ):
     return create_transaction(
         db,
         transaction_data,
-        current_user
+        current_user.id
     )
+    
